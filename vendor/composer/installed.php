1f0713ea195ffcc86f623f2ce488ad3a98e71cc7
Javi_Afellay <javiafellay96@gmail.com>
2022-01-16 22:01:11 -0300

    primer Comiit
    error en el Router.php

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'javiermartinezguanez/app-salon_php_mvc_js_sass',
+  ),
+  'versions' => 
+  array (
+    'javiermartinezguanez/app-salon_php_mvc_js_sass' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
